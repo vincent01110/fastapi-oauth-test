8830271b6dd67f84b1d3540311f5dbc458e77bd5
vincent01110 <bencekristof06@gmail.com>
2025-04-29 16:22:42 +0200

    comments added

--- src/main.py
+++ src/main.py
@@ -5,12 +5,12 @@ from fastapi.responses import RedirectResponse
 import requests
 
 params = {
-    "client_id": CLIENT_ID,
-    "redirect_uri": REDIRECT_URI,
-    "response_type": "code",
-    "scope": "openid email profile",
-    "access_type": "offline",
-    "prompt": "consent"
+    "client_id": CLIENT_ID,  # Client ID from the Google Cloud Auth Platform
+    "redirect_uri": REDIRECT_URI, # Client Secret from the Google Cloud Auth Platform
+    "response_type": "code", # Auth type, 'code' is prefered
+    "scope": "openid email profile", # the date you need access to
+    "access_type": "offline", # means that the google auth api also adds a refresh token
+    "prompt": "consent" # show consent screen
 }
 
 app = FastAPI()
@@ -19,38 +19,47 @@ app = FastAPI()
 def hello():
     return 'Hello World'
 
+
+# the client app fetches this endpoint which redirects to the google auth screen
 @app.get('/auth/login')
 def login():
+    # embeds the params to the urls
     url = "https://accounts.google.com/o/oauth2/v2/auth?" + urllib.parse.urlencode(params)
     
+    # redirects client
     return RedirectResponse(url)
 
-
+# this is where google will redirect the client after it logs in and gives consent
+# the 'code' is nessecery to request additional resources, after consent google embeds
+# it to the request params
 @app.get("/auth/callback")
 def auth_callback(code: str):
+    # endpoint for requesting access tokens
     token_url = "https://oauth2.googleapis.com/token"
 
     data = {
-        "code": code,
-        "client_id": CLIENT_ID,
-        "client_secret": CLIENT_SECRET,
-        "redirect_uri": REDIRECT_URI,
-        "grant_type": "authorization_code"
+        "code": code, # the code that google provides after authentication
+        "client_id": CLIENT_ID, # Client ID from the Google Cloud Auth Platform
+        "client_secret": CLIENT_SECRET, # Client Secret from the Google Cloud Auth Platform
+        "redirect_uri": REDIRECT_URI, # its the redirect uri that you provided on the Google Cloud Platform
+        "grant_type": "authorization_code" # it tells the google api that you provide the 'code'
     }
     
+    # requesting access token
     response = requests.post(token_url, data=data)
     token_data = response.json()
-    
     
     print(token_data)
     
     access_token = token_data['access_token']
     
+    # request definition
     user_info_url = "https://www.googleapis.com/oauth2/v2/userinfo"
     headers = {
         "Authorization": f"Bearer {access_token}"
     }
 
+    # requesting profile information
     user_info_response = requests.get(user_info_url, headers=headers)
     user_info = user_info_response.json()
 
